414eb4d48903b7c4cf2ace5f57d28d1518c25fd9
Avery Behm <darthavery23@gmail.com>
2022-09-03 22:40:32 -0600

    commit tree b9aaa84d0d999b46404cee064144436c40068b7b

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+// Write your solution here!
+// test 1
+const cats = ["Milo", "Otis", "Garfield"];
+// test 2
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+// test 3
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+// test 4
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+// test 5
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+// test 6
+const copyOfCatsAppend = [...cats, "Broom"];
+function appendCat(name) {
+    return copyOfCatsAppend;
+}
+// test 7
+const copyOfCatsPrepend = ["Arnold", ...cats];
+function prependCat(name) {
+    return copyOfCatsPrepend;
+}
+//test 8
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+//test 9
+function removeFirstCat() {
+    return cats.slice(1);
+}
